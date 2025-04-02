1234a5b8604a2d23be1707b1d20dd24ee1b4b349
Patrizio Veglia <pveglia@gmx.de>
2025-04-02 12:44:56 +0200

    Update Amount property comment in Expense model
    Clarified the comment for the Amount property in the Expense model by appending additional placeholder text. This change ensures better explanation or context for potential usage scenarios.

--- WebAssembly.Server/Models/Expense.cs
+++ WebAssembly.Server/Models/Expense.cs
@@ -4,7 +4,7 @@ public class ExpenseDto
 {
     public string Id { get; set; } = Guid.NewGuid().ToString();
     public string Name { get; set; } = "";
-    public string Amount { get; set; } = ""; // z. B. "€42.50"
+    public string Amount { get; set; } = ""; // z. B. "€42.50"// wdadaw
     public DateTime Date { get; set; } 
     public string Category { get; set; } = "";
     
